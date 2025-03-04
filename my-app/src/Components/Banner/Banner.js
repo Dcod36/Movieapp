80020839e40e29e57c4d5f81fd9b3499d2a000c3
devika <devikasanthosh593@gmail.com>
2025-03-05 00:05:33 +0530

    ui created

--- my-app/src/Components/Banner/Banner.js
+++ my-app/src/Components/Banner/Banner.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "../../axios";
+import {API_KEY,imageUrl} from '../../constants/constants'
 import "./Banner.css"
-function Banner() 
-{
+function Banner() {
+const [movie, setMovie] = useState()
+    useEffect(()=>{
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            const movies=response.data.results;
+            const randomIndex=Math.floor(Math.random()*movies.length);
+            console.log(movies[randomIndex])
+            setMovie(movies[randomIndex])
+        })
+        },[])
+ 
     return(
-       <div className="banner">
+        <div
+        style={{ backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})` }}
+
+        className="banner">
         <div className="content">
-            <h1 className="title">Movie name</h1>
-            <div classname="banner_button">
+            <h1 className="title">{movie?movie.title:""}</h1>
+            <div className="banner_button">
                 <button className=" button">Play</button>
                 <button className=" button">My list</button>
             </div>
-            <h1 className="description">Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development to fill empty spaces in a layout that do not yet</h1>  
+            <h1 className="description">{movie?movie.overview:""}</h1>  
         </div>
       <div className="fade-button"></div>
          </div>
---
+++ my-app/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl="https://api.themoviedb.org/3"
+export const API_KEY="9e3f0825cd1fa4c8028aa186844fd362";
+export const imageUrl='https://image.tmdb.org/t/p/original'
