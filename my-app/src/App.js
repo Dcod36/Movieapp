2ac9d4824f3047e65f343e9cf9226b2bf49e597c
devika <devikasanthosh593@gmail.com>
2025-03-08 11:16:47 +0530

    video added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from  'react'
 import './App.css'
-import {action,originals} from './urls'
+import {action,originals,horror,RomanceMovies,Documentaries} from './urls'
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
@@ -12,7 +12,10 @@ function App() {
       <Banner/>     
       <RowPost url={originals} title='Netflix Orginals'/> 
       <RowPost url={action} title='Action' isSmall/>
-      <RowPost url={action} title='Action' isSmall/> 
+      <RowPost url={horror} title='Horror' isSmall/>  
+      <RowPost url={RomanceMovies} title='RomanceMovies' isSmall/>
+      <RowPost url={Documentaries} title='Documentaries' isSmall/>  
+      
           </div>
   );
 }
--- my-app/src/Components/RowPost/RowPost.js
+++ my-app/src/Components/RowPost/RowPost.js
@@ -1,16 +1,35 @@
 import React,{useEffect,useState} from 'react'
 import './RowPost.css'
+import Youtube from 'react-youtube'
 import axios from '../../axios'
-import {imageUrl} from '../../constants/constants'  
+import {API_KEY,imageUrl} from '../../constants/constants'  
 function RowPost(props) {
   const[movies,setmovies]=useState([])  
+  const[urlid,seturlid]=useState('')
   useEffect(() => {
     axios.get(props.url).then(response=>{
       console.log(response.data)
       setmovies(response.data.results)
     }).catch(err=>{alert('Network Error')}) 
   }, [])
-  
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+   const handleMovie=(id)=>{
+    console.log(id)
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+      if(response.data.results.length!==0){
+        seturlid(response.data.results[0])
+    }
+  else{
+    console.log('Array is empty')
+  }})
+   }
   return (
 
     <div className='row'>   
@@ -18,11 +37,12 @@ function RowPost(props) {
       <div className="posters">
         {movies.map((obj)=>
         
-            <img className={props.isSmall? "smallPoster":"poster"} src={`${imageUrl+obj.backdrop_path}`}alt='poster'/>
+            <img onClick={()=>handleMovie(obj.id)}className={props.isSmall? "smallPoster":"poster"} src={`${imageUrl+obj.backdrop_path}`}alt='poster'/>
 
   )}
             
       </div>
+     {urlid && <Youtube  videoId={urlid.key} opts={opts}/> }
     </div>
   )
 }
